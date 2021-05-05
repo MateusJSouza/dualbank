e5a9965ecf56c4ff02622ac7fd26cdf4e3beac62
MateusJesus <mateusjdev@gmail.com>
2021-05-05 19:42:53 -0300

    ✨ add: logo, info da conta e saldo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import logoImg  from '../../assets/logo.svg'
 import copy  from '../../assets/copy.svg'
 import carets  from '../../assets/carets.svg'
+import eye  from '../../assets/eye.svg'
 import styles from './styles.module.scss'
 
 export function Header() {
@@ -9,16 +10,27 @@ export function Header() {
       <div className={styles.headerContainer}>
         <img src={logoImg} alt="dualbank"/>
 
-        <p>Conta Corrent</p>
-        <img src={carets} alt=""/>
-        
-        <p>Ag. 00005-1</p>
-        <p>C.C. 00000000000000000020-1</p>
+        <div className={styles.infos}>
+          <div className={styles.infoAccount}>
+            <p>Conta Corrente</p>
+            <img src={carets} alt="carets"/>
+          </div>
 
-        <img src={copy} alt=""/>
+          <div className={styles.infoAgency}>
+            <p>Ag. 00005-1</p>
+            <span>C.C. 00000000000000000020-1</span>
 
-        <p>Saldo</p>
-        <span>R$40.000,00</span>
+            <img src={copy} alt="copy"/>
+          </div>
+        </div>
+
+        <div className={styles.infoTotal}>
+          <div className={styles.total}>
+            <p>Saldo</p>
+            <img src={eye} alt="eye"/>
+          </div>
+            <span>R$40.000,00</span>
+        </div>
       </div>
     </>
   );
