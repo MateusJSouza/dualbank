1eb8ec6bebf41ee8b5b0d2047e1b195ff79a98c9
MateusJesus <mateusjdev@gmail.com>
2021-05-05 23:46:08 -0300

    🐛 header arrumado

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,35 +1,64 @@
-import logoImg  from '../../assets/logo.svg'
-import copy  from '../../assets/copy.svg'
-import carets  from '../../assets/carets.svg'
-import eye  from '../../assets/eye.svg'
+import logoImg from '../../assets/logo.svg'
+import copy from '../../assets/copy.svg'
+import carets from '../../assets/carets.svg'
+import eye from '../../assets/eye.svg'
+import search from '../../assets/search.svg'
+import chatText from '../../assets/chatText.svg'
+import bell from '../../assets/bell.svg'
+import calendar from '../../assets/calendar.svg'
 import styles from './styles.module.scss'
 
 export function Header() {
   return (
     <>
       <div className={styles.headerContainer}>
-        <img src={logoImg} alt="dualbank"/>
+        <div className={styles.navbar}>
+          <img src={logoImg} alt="dualbank" />
 
-        <div className={styles.infos}>
-          <div className={styles.infoAccount}>
-            <p>Conta Corrente</p>
-            <img src={carets} alt="carets"/>
+          <div className={styles.infos}>
+            <div className={styles.infoAccount}>
+              <p>Conta Corrente</p>
+              <img src={carets} alt="carets" />
+            </div>
+
+            <div className={styles.infoAgency}>
+              <p>Ag. 00005-1</p>
+              <span>C.C. 00000000000000000020-1</span>
+
+              <img src={copy} alt="copy" />
+            </div>
           </div>
 
-          <div className={styles.infoAgency}>
-            <p>Ag. 00005-1</p>
-            <span>C.C. 00000000000000000020-1</span>
-
-            <img src={copy} alt="copy"/>
+          <div className={styles.infoTotal}>
+            <div className={styles.total}>
+              <p>Saldo</p>
+              <img src={eye} alt="eye" />
+            </div>
+            <div className={styles.subtotal}>
+              <span>R$40.000,00</span>
+            </div>
           </div>
         </div>
 
-        <div className={styles.infoTotal}>
-          <div className={styles.total}>
-            <p>Saldo</p>
-            <img src={eye} alt="eye"/>
+        {/* DIREITA*/}
+        <div className={styles.search}>
+          <div className={styles.input}>
+            <input
+              placeholder="Pesquisa"
+              className={styles.inputSearch}
+              type="text"
+            />
+
+            <img src={search} className={styles.searchImg} alt="search" />
           </div>
-            <span>R$40.000,00</span>
+
+          <div className={styles.icons}>
+            <img src={chatText} alt="Chat text" />
+            <img src={calendar} alt="Calendar" />
+            <img src={bell} alt="Bell" />
+          </div>
+
+          <div className={styles.circle}></div>
         </div>
       </div>
     </>
