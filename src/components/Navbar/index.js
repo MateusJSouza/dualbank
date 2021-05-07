a271393f2201beb66a456269f625c5c40fb82e69
MateusJesus <mateusjdev@gmail.com>
2021-05-06 23:22:00 -0300

    ✨ add: submenu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,6 +10,7 @@ import question from '../../assets/question.svg'
 import eye from '../../assets/eye.svg'
 import tag from '../../assets/tag.svg'
 import add from '../../assets/add.svg'
+import caretsTwo from '../../assets/caretsTwo.svg'
 
 import pix from '../../assets/pix.png'
 
@@ -17,43 +18,80 @@ import styles from './styles.module.scss'
 
 export function Navbar() {
   return (
-    <nav className={styles.container}>
-      <ul className={styles.icons}>
-        <li><img src={houseLine} alt="house line" /></li>
-        <li><img src={chartPieSlice} alt="chat pie slice" /></li>
-        <li><img src={chartLineUp} alt="chart line up" /></li>
-        <li><img src={money} alt="money" /></li>
-        <li><img src={handshake} alt="handshake" /></li>
-        <li><img src={pix} alt="house line" /></li>
-        <li><img src={creditCard} alt="credit card" /></li>
-        <li><img src={receipt} alt="receipt" /></li>
-        <li><img src={wrench} alt="wrench" /></li>
-        <li><img src={question} alt="question" /></li>
-      </ul>
+    <>
+      <div className={styles.content}>
+        <nav className={styles.container}>
+          <ul className={styles.icons}>
+            <li><img src={houseLine} alt="house line" /></li>
+            <li><img src={chartPieSlice} alt="chat pie slice" /></li>
+            <li><img src={chartLineUp} alt="chart line up" /></li>
+            <li><img src={money} alt="money" /></li>
+            <li><img src={handshake} alt="handshake" /></li>
+            <li><img src={pix} alt="house line" /></li>
+            <li><img src={creditCard} alt="credit card" /></li>
+            <li><img src={receipt} alt="receipt" /></li>
+            <li><img src={wrench} alt="wrench" /></li>
+            <li><img src={question} alt="question" /></li>
 
-      <div className={styles.submenu}>
-        <ul>
-          <div className={styles.tag}>
-            <li><img src={tag} alt="tag" /></li>
-          </div>
-          <div className={styles.add}>
-            <li><img src={add} alt="to add" /></li>
-          </div>
-        </ul>
-
-        <div className={styles.cards}>
-          <p id={styles.paragraph}>P</p>
-
-          <div className={styles.principal}>
-            <p>Principal</p>
-            <img src={eye} alt="eye" />
-          </div>
-
-          <div className={styles.subtotal}>
-              <span>R$10.000,00</span>
+            <div className={styles.carets}>
+              <img src={caretsTwo} className={styles.caretsTwo} alt="carets" />
             </div>
-        </div>
+          </ul>
+
+          <div className={styles.submenu}>
+            <div className={styles.iconsMenu}>
+              <div className={styles.tag}>
+                <li><img src={tag} alt="tag" /></li>
+              </div>
+              <div className={styles.add}>
+                <li><img src={add} alt="to add" /></li>
+              </div>
+            </div>
+
+            <div className={styles.infoTotal}>
+              <div className={styles.paragraph}>
+                <p>P</p>
+              </div>
+
+              <div className={styles.total}>
+                <p>Saldo</p>
+                <img src={eye} alt="eye" />
+              </div>
+              <div className={styles.subtotal}>
+                <span>R$40.000,00</span>
+              </div>
+            </div>
+
+            <div className={styles.infoRent}>
+              <div className={styles.paragraphTwo}>
+                <p>A</p>
+              </div>
+
+              <div className={styles.rent}>
+                <p>Aluguel</p>
+                <img src={eye} alt="eye" />
+              </div>
+              <div className={styles.rentTotal}>
+                <span>R$1.600,00</span>
+              </div>
+            </div>
+
+            <div className={styles.infoAlimentation}>
+              <div className={styles.paragraphThree}>
+                <p>A</p>
+              </div>
+
+              <div className={styles.alimentation}>
+                <p>Alimentação</p>
+                <img src={eye} alt="eye" />
+              </div>
+              <div className={styles.alimentationTotal}>
+                <span>R$450,00</span>
+              </div>
+            </div>
+          </div>
+        </nav>
       </div>
-    </nav>
+    </>
   );
 }
